b7156ff1c0cc38797251f64e180c016f6c5d8cc1
Ravi Sinha <ravisinha@my.unt.edu>
2026-05-13 20:34:50 -0500

    Update arrow data structure

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ type Direction = { x: 0 | 1 | -1; y: 0 | 1 | -1 }
 
 interface Arrow {
   id: string;
-  cells: { x: number, y: number}[]
+  // Index 0 is the head, index length - 1 is the tail
+  cells: Array<{ x: number, y: number}>;
   direction: Direction;
-  isEscaping: boolean;
+  status: 'idle' | 'moving' | 'escaping' | 'colliding';
 }
