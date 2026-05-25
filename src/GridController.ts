7ab2cd6f18f738ddbaa028f86f2444e7bbb8fd0a
Ravi Sinha <ravisinha@my.unt.edu>
2026-05-24 19:28:21 -0500

    Generate occupancy map with all false

--- src/GridController.ts
+++ src/GridController.ts
@@ -2,9 +2,15 @@ function moveArrow(arrowId: string) {
 
 }
 
-function generateOccupancyMap(): Map<string, string> {
-  // key: "x,y"
-  // value: arrowId
+function generateOccupancyMap(width: number, height: number): Map<string, boolean> {
+  const occupancyMap = new Map<string, boolean>();
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      occupancyMap.set(`${i},${j}`, false)
+    }
+
+    return occupancyMap;
+  }
 }
 
 function checkCollision(arrowId: string) {
